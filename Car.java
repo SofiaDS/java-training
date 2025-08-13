0efb863fe7e1788282a8cb8111bd7d9d00064bcb
sofia_ds <s.dellaspora@outlook.com>
2025-08-13 11:00:24 +0200

    add main method to Car class for execution demonstration

--- Car.java
+++ Car.java
@@ -3,4 +3,9 @@ public class Car {
     String brand;
     String model;
     int year;
+
+    // Entry point for static class execution
+    public static void main(String[] args) {
+        System.out.println("Hello world from a Car class");
+    }
 }
