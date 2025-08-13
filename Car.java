c5c5c0666abc638cbccaf6f26c7ce57824f235c1
sofia_ds <s.dellaspora@outlook.com>
2025-08-13 11:16:22 +0200

    refactor main method to initialize Car objects with attributes

--- Car.java
+++ Car.java
@@ -10,8 +10,18 @@ public class Car {
 
         // instantiate a Car object
         Car car1 = new Car();
+        car1.brand = "Toyota";
+        car1.model = "Corolla";
+        car1.year = 2020;
+
         Car car2 = new Car();
+        car2.brand = "Honda";
+        car2.model = "Civic";
+        car2.year = 2021;
         Car car3 = new Car();
+        car3.brand = "Ford";
+        car3.model = "Focus";
+        car3.year = 2019;
 
         System.out.println("Car 1: " + car1.brand + " " + car1.model + " (" + car1.year + ")");
         System.out.println("Car 2: " + car2.brand + " " + car2.model + " (" + car2.year + ")");
