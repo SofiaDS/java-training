586d25438ca4ef01e3aaaca02761ceb4014e95d0
sofia_ds <s.dellaspora@outlook.com>
2025-08-13 11:14:12 +0200

    implement main method to instantiate Car objects and display their attributes

--- Car.java
+++ Car.java
@@ -7,5 +7,14 @@ public class Car {
     // Entry point for static class execution
     public static void main(String[] args) {
         System.out.println("Hello world from a Car class");
+
+        // instantiate a Car object
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+
+        System.out.println("Car 1: " + car1.brand + " " + car1.model + " (" + car1.year + ")");
+        System.out.println("Car 2: " + car2.brand + " " + car2.model + " (" + car2.year + ")");
+        System.out.println("Car 3: " + car3.brand + " " + car3.model + " (" + car3.year + ")");
     }
 }
