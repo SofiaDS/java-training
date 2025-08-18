4ed08452c7459c354ba6537b4b2f75b63bb34d0d
sofia_ds <s.dellaspora@outlook.com>
2025-08-18 17:38:44 +0200

    feat: implement static method to calculate and print the factorial of a positive integer

--- excercises/cl16.java
+++ excercises/cl16.java
@@ -6,4 +6,13 @@ prints its factorial
  */
 public class cl16 {
 
+    public static void main(String[] args) {
+        int positiveNumInt = (int) (Math.random() * 20) + 1;
+
+        long factorial = 1;
+        for (int i = 1; i <= positiveNumInt; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println("Factorial of number " + positiveNumInt + " is " + factorial);
+    }
 }
